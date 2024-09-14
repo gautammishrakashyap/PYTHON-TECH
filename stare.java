414a5172a0140642a7b7f8f6304f9ff572cbf134
gautammishrakashyap <kumargautam7071@gmail.com>
2024-09-15 01:45:47 +0530

    chng

---
+++ stare.java
@@ -0,0 +1,17 @@
+public class stare {
+    public static void main(String[] args) {
+        int n = 19;
+        if(n<18){
+            System.out.print("you can't vote");
+        }
+        else if (n==18) {
+            System.out.println("try kro");
+            
+        } else {
+            System.out.println("enjoy your vote sir");
+
+            
+        }
+    }
+    
+}
